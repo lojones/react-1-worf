ea7488ab1bccef296510300c3f3e6c593ebfc303
lojones <login.jones@gmail.com>
2022-01-09 15:20:57 -0500

    genesis

---
+++ src/components/Topnavbar/Topnavbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './topnavbar.css'
+
+const Topnavbar = () => (
+    <div class="topnav">
+        <a href="#home">Home</a>
+    </div>
+)
+
+export default Topnavbar;
